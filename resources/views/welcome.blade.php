4d63254add0114c1a78b5266dc2db2144ea89cbb
Dean Blackborough <dean@g3d-development.com>
2019-04-12 15:32:35 +0100

    Setup
    * Initial setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <title>Hello, world!</title>
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
+        <div class="container-fluid container-cte">
+            <div class="row container-row">
+                <div class="col-xl-2 col-lg-3 col-md-3 container-column container-left d-none d-sm-none d-md-block">
+                    <h1>Hello, world!</h1>
+                </div>
+                <div class="col-xl-10 col-lg-9 col-md-9 col-sm-12 col-12 container-column container-right d-block">
                     <h1>Hello, world!</h1>
                 </div>
             </div>
