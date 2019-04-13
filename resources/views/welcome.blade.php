a36c98fbbd8f7fdb0ee69f63efbd9b6ac6e3745e
Dean Blackborough <dean@g3d-development.com>
2019-04-13 12:50:36 +0100

    Dashboard
    * Dashboard layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,43 @@
         <div class="container-fluid container-cte">
             <div class="row container-row">
                 <div class="col-xl-2 col-lg-3 col-md-3 container-column container-left d-none d-sm-none d-md-block">
-                    <h1>Hello, world!</h1>
+                    <div class="logo">
+                        <img src="{{ asset('images/theme/logo-128.png') }}" width="64" height="64" alt="Logo" title="Costs to Expect" />
+                    </div>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
                 </div>
                 <div class="col-xl-10 col-lg-9 col-md-9 col-sm-12 col-12 container-column container-right d-block">
-                    <h1>Hello, world!</h1>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="screen-intro">
+                                <div class="icon">
+                                    <img src="{{ asset('images/theme/dashboard.png') }}" width="32" height="32" alt="Screen icon" title="Dashboard" />
+                                </div>
+                                <div class="welcome">
+                                    <small class="text-muted">Welcome to Costs to Expect.com</small>
+                                </div>
+                                <div class="title">
+                                    <h2>The Dashboard</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
