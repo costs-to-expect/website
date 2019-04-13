87ebf03ebcdc7484ce040fe5e6f0053b92054cd5
Dean Blackborough <dean@g3d-development.com>
2019-04-13 13:27:56 +0100

    Dashboard
    * Initial work on the dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,19 +17,19 @@
                     </div>
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Active</a>
+                            <span class="nav-title">The Blackborough Children</span>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Active</a>
+                            <a class="nav-link active" href="#">Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">Jack James</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">Pending</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled menu item</a>
                         </li>
                     </ul>
                 </div>
@@ -45,6 +45,33 @@
                                 </div>
                                 <div class="title">
                                     <h2>The Dashboard</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <h5>Total expenses to date</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="media summary-block shadow-sm rounded">
+                                <img src="{{ asset('images/theme/dashboard.png') }}" class="mr-3" width="32" height="32" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Jack James Blackborough</h4>
+                                    <h6 class="mt-0">Total expenses <small class="text-muted">From birth 28th June 2013</small></h6>
+                                    <p class="total">&pound;39,951.29</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="media summary-block shadow-sm rounded">
+                                <img src="{{ asset('images/theme/dashboard.png') }}" class="mr-3" width="32" height="32" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Pending Blackborough</h4>
+                                    <h6 class="mt-0">Total expenses <small class="text-muted">From birth xx April/May 2019</small></h6>
+                                    <p class="total">&pound;484.42</p>
                                 </div>
                             </div>
                         </div>
