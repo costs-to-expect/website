ca52cea2ef39ba79679536ceaab7d079544dca70
Dean Blackborough <dean@g3d-development.com>
2019-04-13 16:00:46 +0100

    Dashboard
    * Dashboard updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <title>Hello, world!</title>
+        <title>Costs to Expect.com</title>
     </head>
     <body>
         <div class="container-fluid container-cte">
@@ -23,7 +23,7 @@
                             <a class="nav-link active" href="#">Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Jack James</a>
+                            <a class="nav-link" href="#">Jack</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Pending</a>
@@ -51,27 +51,74 @@
                     </div>
                     <div class="row mt-4">
                         <div class="col-12">
-                            <h5>Total expenses to date</h5>
+                            <h4>Total expenses to date</h4>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-                            <div class="media summary-block shadow-sm rounded">
-                                <img src="{{ asset('images/theme/dashboard.png') }}" class="mr-3" width="32" height="32" alt="icon">
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
                                 <div class="media-body">
-                                    <h4 class="mt-0">Jack James Blackborough</h4>
+                                    <h4 class="mt-0">Jack Blackborough</h4>
                                     <h6 class="mt-0">Total expenses <small class="text-muted">From birth 28th June 2013</small></h6>
-                                    <p class="total">&pound;39,951.29</p>
+                                    <p class="total mb-0">&pound;39,951.29</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-                            <div class="media summary-block shadow-sm rounded">
-                                <img src="{{ asset('images/theme/dashboard.png') }}" class="mr-3" width="32" height="32" alt="icon">
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
                                 <div class="media-body">
                                     <h4 class="mt-0">Pending Blackborough</h4>
-                                    <h6 class="mt-0">Total expenses <small class="text-muted">From birth xx April/May 2019</small></h6>
-                                    <p class="total">&pound;484.42</p>
+                                    <h6 class="mt-0">Total expenses <small class="text-muted">From birth xx April 2019</small></h6>
+                                    <p class="total mb-0">&pound;484.42</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Blackborough Children</h4>
+                                    <h6 class="mt-0">Total expenses <small class="text-muted">Both children</small></h6>
+                                    <p class="total mb-0">&pound;40,435.71</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <h4>Expenses for current year (2019)</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Jack Blackborough</h4>
+                                    <h6 class="mt-0">2019 <small class="text-muted">Expenses in 2019</small></h6>
+                                    <p class="total mb-0">&pound;1,121.19</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Pending Blackborough</h4>
+                                    <h6 class="mt-0">2019 <small class="text-muted">Expenses in 2019</small></h6>
+                                    <p class="total mb-0">&pound;484.42</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-4" style="margin-bottom: 1rem;">
+                            <div class="media summary-block shadow-sm h-100">
+                                <img src="{{ asset('images/theme/expenses.png') }}" class="mr-2" width="48" height="48" alt="icon">
+                                <div class="media-body">
+                                    <h4 class="mt-0">Blackborough Children</h4>
+                                    <h6 class="mt-0">2019 <small class="text-muted">Total expenses in 2019</small></h6>
+                                    <p class="total mb-0">&pound;1,605.61</p>
                                 </div>
                             </div>
                         </div>
