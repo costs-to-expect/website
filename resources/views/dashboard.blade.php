abac5782cfe71667b8188dce88a7405e1f13eda6
Dean Blackborough <dean@g3d-development.com>
2019-05-05 13:44:40 +0100

    Menu definition
    * Menu definition

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,7 +45,7 @@
                             <a class="nav-link" href="/what-we-count">What we count?</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link no-icon" href="/changelog">Changelog</a>
+                            <a class="nav-link" href="/changelog">Changelog</a>
                         </li>
                     </ul>
                 </div>
