94971ce867b94d3fb6b7d70bb70ddcc60ec89021
Dean Blackborough <dean@g3d-development.com>
2019-05-09 15:08:13 +0100

    Release
    * New release, v1.02.0

--- config/web/app.php
+++ config/web/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'release' => 'v1.02.0',
-    'date' => '6th May 2019',
+    'date' => '9th May 2019',
     'copyright' => 'Dean Blackborough 2018 - 2019',
     'api-link' => 'https://api.costs-to-expect.com'
 ];
