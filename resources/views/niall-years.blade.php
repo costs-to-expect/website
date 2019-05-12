c27be89e0bf550a2dd402056b815d38938c27a13
Dean Blackborough <dean@g3d-development.com>
2019-05-12 09:59:40 +0100

    Pagination view component
    - Working on a pagination view component

--- resources/views/niall-years.blade.php
+++ resources/views/niall-years.blade.php
@@ -158,6 +158,11 @@
                     </div>
                     <div class="row">
                         <div class="col-12">
+                            @include(
+                                'component.table-pagination',
+                                [
+                                ]
+                            )
                             <div class="table-pagination p-3">
                                 <div class="row">
                                     <div class="pages col-7 col-sm-10 col-xl-11"><a class="btn btn-primary disabled"><</a> <a class="btn btn-primary d-none d-sm-inline-block">1</a> <a class="btn btn-primary active">2</a> <a class="btn btn-primary d-none d-sm-inline-block">3</a> <a class="btn btn-primary">></a></div>
