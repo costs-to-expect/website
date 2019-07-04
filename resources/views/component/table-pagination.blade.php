a4efbfb1aa3384e07ccfa940d233f2719107c03b
Dean Blackborough <dean@g3d-development.com>
2019-07-05 00:45:38 +0100

    Minor correction to pagination on mobile.
    - Minor correction to pagination on mobile.

--- resources/views/component/table-pagination.blade.php
+++ resources/views/component/table-pagination.blade.php
@@ -1,6 +1,6 @@
 <div class="table-pagination p-3">
     <div class="row">
-        <div class="pages col-7 col-sm-10 col-xl-11">
+        <div class="pages col-9 col-sm-10 col-xl-11">
             @if ($offset > 0)
             <a href="{{ $uri['base'] }}?offset={{ $offset - $limit }}&limit={{ $limit }}{{ $uri['parameters'] }}#expenses-data" class="btn btn-primary"><</a>
             @else
@@ -23,7 +23,7 @@
             <a class="btn btn-primary disabled">></a>
             @endif
         </div>
-        <div class="per-page col-5 col-sm-2 col-xl-1">
+        <div class="per-page col-3 col-sm-2 col-xl-1">
             <form class="form-inline">
                 <select class="form-control form-control-sm per-page" data-uri="{{ $uri['base'] }}">
                     @foreach ($limit_options as $option)
