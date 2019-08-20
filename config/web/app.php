8fc37d76c2eb4402c21585562dfdb481406bc71d
Dean Blackborough <dean@g3d-development.com>
2019-08-20 23:16:09 +0100

    Changed copyright
    - Changed the copyright to G3D Development.
    - Added copyright_url as a config value.

--- config/web/app.php
+++ config/web/app.php
@@ -6,6 +6,7 @@ return [
     'release' => 'v1.09.2',
     'date' => 'xxth August 2019',
     'copyright' => 'G3D Development Limited 2018 - 2019',
+    'copyright_url' => 'https://www.g3d-development.com',
     'api-link' => 'https://api.costs-to-expect.com',
     'api-source' => ENV('API_SOURCE')
 ];
