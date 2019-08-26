c1e67d25fb5288e99e831badacdc235e4ef0b630
Dean Blackborough <dean@g3d-development.com>
2019-08-27 00:08:08 +0100

    About page content updated
    - Updated the text for the about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,12 +6,35 @@
     <div class="col-12">
         <h2>In short</h2>
 
-        <p>Costs to Expect is a service which will allow tracking and forecasting of any expenses, be
-            they the costs to raise a child to the age of 18 or your Business account budget for the
-            next financial year.</p>
+        <p>Costs to Expect is a service that focuses on tracking and
+            forecasting expenses. The service is not going to be limited to
+            expenses; however, we figured that it was an excellent place to
+            start.</p>
 
-        <p>The public side of Costs to Expect tracks the expenses involved in raising a child to the
-            age of 18 in the UK.</p>
+        <hr />
+
+        <h2>Service?</h2>
+
+        <p>There are three parts to Costs to Expect, the Open Source API, the
+            backbone, the app which contains our secret sauce and the website.
+            The website gives a small example of the service; we are tracking
+            the costs to raise our children to adulthood.</p>
+
+        <hr />
+
+        <h2>Short term goals</h2>
+
+        <p>In 2019, early 2020, the app will open to the public; initially, it
+            will provide budgeting features enabling you to manage your annual
+            budgeting needs easily.</p>
+
+        <p>Later in 2020, the service will expand in two core ways. The
+            budgeting and forecasting features will be expanded to support
+            much more complex account setups, for example, businesses.
+            Secondly, we will enable customisation. With our personalisation,
+            you will be able to set up the service to track any 'item'. You
+            will be able to build your data structure rather than being limited
+            to our primary data types?</p>
 
         <hr />
 
@@ -19,31 +42,22 @@
 
         <h3>What does it cost to raise a child in the UK?</h3>
 
-        <p>Costs to Expect is a long-term project, my wife and I are tracking the
-            expenses to raise our children to adulthood, 18.</p>
+        <p>Costs to Expect is a long-term project; my wife and I are tracking
+            the expenses to raise our children to adulthood, 18.</p>
 
         <h3>Why?</h3>
 
-        <p>There are core reasons why we have chosen to do this. I love data, the more then
-            merrier, secondly, for as long I remember, it appears to have become accepted knowledge
-            that it costs approximately £250,000 to raise a child in the UK.</p>
+        <p>There are two core reasons. Firstly,  I love data, the more then
+            merrier. Secondly, for as long I remember, it appears to have
+            become accepted knowledge that it costs approximately £250,000 to
+            raise a child in the UK.</p>
 
-        <p>If you really think about that number, it becomes apparent very quickly that it just
-            can't be correct for the majority of the UK, on average, over £10k a year?</p>
-
-        <hr />
-
-        <h2>The service</h2>
-
-        <h3>Budgeting and Forecasting</h3>
-
-        <p>In addition to tracking historic costs the Costs to Expect service will include
-            forecasting and budgeting tools, more details will be provided as the service expands.
-            We have exciting plans for 2019 and 2020 and look forward to releasing the first
-            version of the service before the year is out.</p>
+        <p>If you think about that number, it becomes apparent very quickly
+            that it just can't be correct for the majority of the UK, on
+            average, over £10k a year?</p>
 
         <p class="text-muted text-right">Costs to Expect Team</p>
-        <p class="text-muted small text-right">April 2019</p>
+        <p class="text-muted small text-right">August 2019</p>
     </div>
 </div>
 
