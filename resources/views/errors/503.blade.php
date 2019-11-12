3ba776678471e0e3200f0876d6e523c9d738ce1c
Dean Blackborough <dean@g3d-development.com>
2019-11-12 12:07:54 +0000

    Modifiy 503 error page
    - Modify test for 503 error

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::layout')
 
-@section('title', __('Service Unavailable'))
+@section('title', __('Costs to Expect is unavailable'))
 @section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+@section('message', __($exception->getMessage() ?: 'Costs to Expect is unavailable, a new release is in progress'))
