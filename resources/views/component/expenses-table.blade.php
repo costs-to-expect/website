cd2acea604d884d82e955c52da2b9046e580bdc4
Dean Blackborough <dean@g3d-development.com>
2020-10-08 10:05:50 +0100

    Support v2.15
    - Tweak the look of categories.
    - Update all tables based on the new category changes.

--- resources/views/component/expenses-table.blade.php
+++ resources/views/component/expenses-table.blade.php
@@ -4,8 +4,7 @@
         <tr>
             <th scope="col">Expense</th>
             <th scope="col">Date</th>
-            <th scope="col" class="d-none d-md-table-cell">Category</th>
-            <th scope="col" class="d-none d-md-table-cell">Subcategory</th>
+            <th scope="col" class="d-none d-md-table-cell">Categories</th>
             <th scope="col" class="d-none d-xl-table-cell">Total</th>
             <th scope="col" class="d-none d-xl-table-cell">Allocation</th>
             <th scope="col">Amount</th>
@@ -16,23 +15,19 @@
         <tr class="top">
             <td>{{ $expense['name'] }}</td>
             <td><span class="d-none d-md-block">{{ date('j M Y', strtotime($expense['effective_date'])) }}</span><span class="d-table-cell d-sm-block d-md-none">{{ date('d/m/Y', strtotime($expense['effective_date'])) }}</span></td>
+
             <td class="d-none d-md-table-cell">
-                <span class="category">
-                    @if ($expense['category'] !== null && array_key_exists('category_id', $expense['category']))
-                    <a href="{{ $base_uri . '/expenses?category=' . $expense['category']['category_id'] }}">{{ $expense['category']['name'] }}</a>
-                    @else
-                    Not set
-                    @endif
-                </span>
-            </td>
-            <td class="d-none d-md-table-cell">
-                <span class="category">
-                    @if ($expense['category'] !== null && array_key_exists('category_id', $expense['category']) && $expense['subcategory'] !== null && array_key_exists('subcategory_id', $expense['subcategory']))
-                    <a href="{{ $base_uri . '/expenses?category=' . $expense['category']['category_id'] . '&subcategory=' . $expense['subcategory']['subcategory_id'] }}">{{ $expense['subcategory']['name'] }}</a>
-                    @else
-                    Not set
-                    @endif
-                </span>
+                @foreach ($expense['categories'] as $category)
+                    <span class="category">
+                                    <a href="{{ $base_uri . '/expenses?category=' . $category['category_id'] }}">{{ $category['name'] }}</a>
+                                </span>
+
+                    @foreach ($category['subcategories'] as $subcategory)
+                        <span class="subcategory">
+                            / <a href="{{ $base_uri . '/expenses?category=' . $category['category_id'] . '&subcategory=' . $subcategory['subcategory_id']}}">{{ $subcategory['name'] }}</a>
+                        </span>
+                    @endforeach
+                @endforeach
             </td>
             <td class="d-none d-xl-table-cell">£{{ $expense['total'] }}</td>
             <td class="d-none d-xl-table-cell">{{ $expense['percentage'] }}%</td>
